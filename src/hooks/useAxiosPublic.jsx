0f5ae56354d10e074813b5b4d1cb0b564d673cec
alifhasanshah <alifhasanshah@gmail.com>
2023-11-30 22:16:27 +0600

    vercel server link added to axios

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosPublic = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://tikidocs-server.vercel.app/api/v1',
  
 });
 const useAxiosPublic = () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://tikidocs-server.vercel.app/api/v1",
 });
 
 const useAxiosSecure = () => {
